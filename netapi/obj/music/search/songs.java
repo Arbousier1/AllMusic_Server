224cb60fe793740853fc2948740459de76aa36b1
Arbousier1 <elderli@foxmail.com>
2026-03-10 11:12:03 +0800

    Guard netease search artist parsing

--- netapi/obj/music/search/songs.java
+++ netapi/obj/music/search/songs.java
@@ -17,13 +17,25 @@ public class songs {
     }
 
     public String getAlbum() {
+        if (album == null || album.getName() == null) {
+            return "";
+        }
         return album.getName();
     }
 
     public String getArtists() {
         StringBuilder a = new StringBuilder();
+        if (artists == null || artists.isEmpty()) {
+            return "";
+        }
         for (artists temp : artists) {
+            if (temp == null || temp.getName() == null || temp.getName().isEmpty()) {
+                continue;
+            }
             a.append(temp.getName()).append(",");
+        }
+        if (a.length() == 0) {
+            return "";
         }
         return a.substring(0, a.length() - 1);
     }
